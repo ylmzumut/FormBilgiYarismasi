a98bfc9d379cbe4dc5dd17ef637c1d4a2f51a3a4
Umut Yılmaz <umut1907kanarya@gmail.com>
2021-12-26 23:45:10 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Yazılım_yarisma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxSoru = new System.Windows.Forms.TextBox();
+            this.buttonA = new System.Windows.Forms.Button();
+            this.buttonC = new System.Windows.Forms.Button();
+            this.buttonB = new System.Windows.Forms.Button();
+            this.buttonD = new System.Windows.Forms.Button();
+            this.buttonbasla = new System.Windows.Forms.Button();
+            this.lbldogru = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSoru
+            // 
+            this.textBoxSoru.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxSoru.BackColor = System.Drawing.Color.LightCyan;
+            this.textBoxSoru.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSoru.Location = new System.Drawing.Point(216, 28);
+            this.textBoxSoru.Multiline = true;
+            this.textBoxSoru.Name = "textBoxSoru";
+            this.textBoxSoru.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxSoru.Size = new System.Drawing.Size(545, 196);
+            this.textBoxSoru.TabIndex = 0;
+            this.textBoxSoru.Text = "Bilgi Yarışmasına Hoşgeldiniz";
+            this.textBoxSoru.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxSoru.UseWaitCursor = true;
+            // 
+            // buttonA
+            // 
+            this.buttonA.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonA.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonA.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonA.Location = new System.Drawing.Point(216, 254);
+            this.buttonA.Name = "buttonA";
+            this.buttonA.Size = new System.Drawing.Size(253, 89);
+            this.buttonA.TabIndex = 1;
+            this.buttonA.Text = "A";
+            this.buttonA.UseVisualStyleBackColor = false;
+            this.buttonA.Click += new System.EventHandler(this.buttonA_Click);
+            // 
+            // buttonC
+            // 
+            this.buttonC.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonC.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonC.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonC.Location = new System.Drawing.Point(216, 373);
+            this.buttonC.Name = "buttonC";
+            this.buttonC.Size = new System.Drawing.Size(253, 89);
+            this.buttonC.TabIndex = 2;
+            this.buttonC.Text = "C";
+            this.buttonC.UseVisualStyleBackColor = false;
+            this.buttonC.Click += new System.EventHandler(this.buttonC_Click);
+            // 
+            // buttonB
+            // 
+            this.buttonB.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonB.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonB.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonB.Location = new System.Drawing.Point(508, 254);
+            this.buttonB.Name = "buttonB";
+            this.buttonB.Size = new System.Drawing.Size(253, 89);
+            this.buttonB.TabIndex = 3;
+            this.buttonB.Text = "B";
+            this.buttonB.UseVisualStyleBackColor = false;
+            this.buttonB.Click += new System.EventHandler(this.buttonB_Click);
+            // 
+            // buttonD
+            // 
+            this.buttonD.BackColor = System.Drawing.Color.LightCyan;
+            this.buttonD.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonD.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonD.Location = new System.Drawing.Point(508, 373);
+            this.buttonD.Name = "buttonD";
+            this.buttonD.Size = new System.Drawing.Size(253, 89);
+            this.buttonD.TabIndex = 4;
+            this.buttonD.Text = "D";
+            this.buttonD.UseVisualStyleBackColor = false;
+            this.buttonD.Click += new System.EventHandler(this.buttonD_Click);
+            // 
+            // buttonbasla
+            // 
+            this.buttonbasla.BackColor = System.Drawing.Color.Gold;
+            this.buttonbasla.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.buttonbasla.FlatAppearance.BorderSize = 0;
+            this.buttonbasla.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonbasla.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonbasla.Location = new System.Drawing.Point(791, 205);
+            this.buttonbasla.Name = "buttonbasla";
+            this.buttonbasla.Size = new System.Drawing.Size(169, 92);
+            this.buttonbasla.TabIndex = 5;
+            this.buttonbasla.Text = "Başla";
+            this.buttonbasla.UseVisualStyleBackColor = false;
+            this.buttonbasla.Click += new System.EventHandler(this.buttonbasla_Click);
+            // 
+            // lbldogru
+            // 
+            this.lbldogru.AutoSize = true;
+            this.lbldogru.Location = new System.Drawing.Point(840, 79);
+            this.lbldogru.Name = "lbldogru";
+            this.lbldogru.Size = new System.Drawing.Size(59, 17);
+            this.lbldogru.TabIndex = 6;
+            this.lbldogru.Text = "lbldogru";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(988, 505);
+            this.Controls.Add(this.lbldogru);
+            this.Controls.Add(this.buttonbasla);
+            this.Controls.Add(this.buttonD);
+            this.Controls.Add(this.buttonB);
+            this.Controls.Add(this.buttonC);
+            this.Controls.Add(this.buttonA);
+            this.Controls.Add(this.textBoxSoru);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bilgi Yarışması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSoru;
+        private System.Windows.Forms.Button buttonA;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonB;
+        private System.Windows.Forms.Button buttonD;
+        private System.Windows.Forms.Button buttonbasla;
+        private System.Windows.Forms.Label lbldogru;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yazılım_yarisma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglan = new SqlConnection("Data Source=.;Initial Catalog=yarisma;Integrated Security=True");
+
+        int sayac = 0 , sayi;
+        int syc = 0;
+        int Dogrusayisi = 0;
+        int[] sayilar = new int[10];
+        string s1;
+        Random rnd = new Random();
+        private void buttonbasla_Click(object sender, EventArgs e)
+        {
+            buttonA.BackColor = Color.LightCyan;
+            buttonB.BackColor = Color.LightCyan;
+            buttonC.BackColor = Color.LightCyan;
+            buttonD.BackColor = Color.LightCyan;
+            buttonbasla.Enabled = false;
+            buttonA.Enabled = true;
+            buttonB.Enabled = true;
+            buttonC.Enabled = true;
+            buttonD.Enabled = true;
+            buttonbasla.Text = "Sonraki";
+            sayac++;
+            while (syc < 10)
+            {
+                sayi = rnd.Next(1, 40);
+                if (Array.IndexOf(sayilar, sayi) == -1)
+                {
+                    sayilar[syc] = sayi;
+                    syc++;
+                }
+            }
+
+            if (sayac == 1)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[0], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                    {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 2)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[1], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 3)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[2], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 4)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[3], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 5)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[4], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 6)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[5], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 7)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[6], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 8)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[7], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 9)
+            {
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[8], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 10)
+            {
+                buttonbasla.Text = "Oyunu Bitir";
+                buttonbasla.Enabled = false;
+                
+                baglan.Open();
+                SqlCommand komut = new SqlCommand("Select * from Sorular where SoruID=" + sayilar[9], baglan);
+                SqlDataReader oku = komut.ExecuteReader();
+                while (oku.Read())
+                {
+                    buttonA.Text = (oku["A"].ToString());
+                    buttonB.Text = (oku["B"].ToString());
+                    buttonC.Text = (oku["C"].ToString());
+                    buttonD.Text = (oku["D"].ToString());
+                    textBoxSoru.Text = sayac.ToString() + "." + (oku["Soru"].ToString());
+                    lbldogru.Text = (oku["DogruCevap"].ToString());
+                }
+                baglan.Close();
+            }
+            if (sayac == 11)
+            {
+                textBoxSoru.Text = Environment.NewLine + "UMUT YILMAZ" + Environment.NewLine + "170106206018" + Environment.NewLine + "Oyunu Oynadığınız İçin Teşekkürler:)";
+                buttonA.Enabled = false;
+                buttonB.Enabled = false;
+                buttonC.Enabled = false;
+                buttonD.Enabled = false;
+                buttonA.Text = "DOĞRU:";
+                buttonA.BackColor = Color.ForestGreen;
+                buttonB.Text = Dogrusayisi.ToString();
+                buttonB.BackColor = Color.ForestGreen;
+                buttonC.Text = "YANLIŞ";
+                buttonC.BackColor = Color.Red;
+                buttonD.Text = (10-Dogrusayisi).ToString();
+                buttonD.BackColor = Color.Red;
+                buttonbasla.Enabled = false;
+                buttonbasla.Text = "YİNE BEKLERİZ";
+                if (Dogrusayisi >= 6)
+                {
+                    MessageBox.Show("TEBRİKLER GÜZEL SONUÇ:)"); 
+                }
+                if (Dogrusayisi < 6) 
+                {
+                    MessageBox.Show("BİR DAHAKİNE DAHA İYİSİNİ YAP;)"); 
+                }
+            }
+        }
+
+
+        private void buttonA_Click(object sender, EventArgs e)
+        {
+            if (buttonA.Text== lbldogru.Text)
+            {
+                Dogrusayisi += 1;
+                buttonA.BackColor = Color.Green;
+            }
+            else
+            {
+                buttonA.BackColor = Color.Red;
+                if (buttonD.Text == lbldogru.Text)
+                {
+                    buttonD.BackColor = Color.Green;
+                }
+                if (buttonB.Text == lbldogru.Text)
+                {
+                    buttonB.BackColor = Color.Green;
+                }
+                if (buttonC.Text == lbldogru.Text)
+                {
+                    buttonC.BackColor = Color.Green;
+                }
+            }
+            buttonbasla.Enabled = true;
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+        private void buttonB_Click(object sender, EventArgs e)
+        {
+            if (buttonB.Text == lbldogru.Text)
+            {
+                Dogrusayisi += 1;
+                buttonB.BackColor = Color.Green;
+            }
+            else
+            {
+                buttonB.BackColor = Color.Red;
+                if (buttonA.Text == lbldogru.Text)
+                {
+                    buttonA.BackColor = Color.Green;
+                }
+                if (buttonD.Text == lbldogru.Text)
+                {
+                    buttonD.BackColor = Color.Green;
+                }
+                if (buttonC.Text == lbldogru.Text)
+                {
+                    buttonC.BackColor = Color.Green;
+                }
+            }
+            buttonbasla.Enabled = true;
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            if (buttonC.Text == lbldogru.Text)
+            {
+                Dogrusayisi += 1;
+                buttonC.BackColor = Color.Green;
+            }
+            else
+            {
+                buttonC.BackColor = Color.Red;
+                if (buttonA.Text == lbldogru.Text)
+                {
+                    buttonA.BackColor = Color.Green;
+                }
+                if (buttonB.Text == lbldogru.Text)
+                {
+                    buttonB.BackColor = Color.Green;
+                }
+                if (buttonD.Text == lbldogru.Text)
+                {
+                    buttonD.BackColor = Color.Green;
+                }
+            }
+            buttonbasla.Enabled = true;
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+
+        
+        private void buttonD_Click(object sender, EventArgs e)
+        {
+            if (buttonD.Text == lbldogru.Text)
+            {
+                Dogrusayisi += 1;
+                buttonD.BackColor = Color.Green;
+            }
+            else
+            {
+                buttonD.BackColor = Color.Red;
+                if (buttonA.Text == lbldogru.Text)
+                { 
+                    buttonA.BackColor = Color.Green;
+                }
+                if (buttonB.Text == lbldogru.Text)
+                {
+                    buttonB.BackColor = Color.Green;
+                }
+                if (buttonC.Text == lbldogru.Text)
+                {
+                    buttonC.BackColor = Color.Green;
+                }
+            }
+            buttonbasla.Enabled = true;
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbldogru.Visible = false;
+            buttonA.Enabled = false;
+            buttonB.Enabled = false;
+            buttonC.Enabled = false;
+            buttonD.Enabled = false;
+        }
+
+    }
+}
